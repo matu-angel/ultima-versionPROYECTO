a94d960d5770c8af3facd4dd359b87f9e3840904
matu-angel <mattiasvelazquez@gmail.com>
2022-08-17 23:24:11 -0300

    version 2

---
+++ fundacion/fundacion/settings/local.py
@@ -0,0 +1,14 @@
+from .base import *      
+# Database
+# https://docs.djangoproject.com/en/4.1/ref/settings/#databases
+
+DATABASES = {
+      ' default ':{
+        'ENGINE':'django.db.backends.mysql',
+        'NAME': 'sitioweb',
+        'USER': 'Test',
+        'PASSWORD':'',
+        'HOST':'localhost',
+        'PORT':'3306',
+    }  
+} 
